3cfe7ea7cd8967795f5d7ccd1c2636e2b9ac2580
Shi Hao Ng <shng2025@gmail.com>
2024-08-12 17:56:31 +0800

    Part 5 code written. just needa get notes down to understand whats going on

--- tdl-app/src/components/AddNewTodo.js
+++ tdl-app/src/components/AddNewTodo.js
@@ -1,10 +1,26 @@
-import React from "react"
+import React, {useState} from 'react'
+import Modal from './Modal'
 
-function AddNewTodo({ children }) {
+
+function AddNewTodo(){
+    const [showModal, setShowModal] = useState(false)
 
     return (
-        <div className="AddNewTodo">
-            AddNewTodo
+        <div className='AddNewTodo'>
+            <div className="btn">
+                <button onClick={() => setShowModal(true)}>
+                    + New Todo
+                </button>
+            </div>
+            <Modal showModal={showModal} setShowModal={setShowModal}>
+                <div>
+                    Hello World
+                    <button
+                    onClick={() => setShowModal(false)}>
+                        hide
+                    </button>
+                </div>
+            </Modal>
         </div>
     )
 }
