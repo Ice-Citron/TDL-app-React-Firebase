1b79bd31099fd9c443e5da3c7b9a016d9e0e8550
Shi Hao Ng <shng2025@gmail.com>
2024-08-13 15:30:26 +0800

    progress for now

--- tdl-app/src/components/AddNewTodo.js
+++ tdl-app/src/components/AddNewTodo.js
@@ -1,20 +1,23 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react' // import useState
+
 import Modal from './Modal'
 
 
 function AddNewTodo(){
+    // showModal = default variable of false. This will be used to show or hide the modal
+    // setShowModal = function that will be used to update the state of "showModal"
     const [showModal, setShowModal] = useState(false)
 
-    return (
-        <div className='AddNewTodo'>
-            <div className="btn">
-                <button onClick={() => setShowModal(true)}>
-                    + New Todo
+    return ( // begins the JSX statement for rendering the modal's UI
+        <div className='AddNewTodo'>                            {/*outer container for "AddNewTodo" component. It uses className, which is for reference during styling in css*/}
+            <div className="btn">                               {/*inner container for button. It uses className, which is for reference during styling in css*/}
+                <button onClick={() => setShowModal(true)}>     {/*button that will set "showModal" to true. It uses onClick to trigger the function setShowModal*/}
+                    + New Todo 
                 </button>
             </div>
-            <Modal showModal={showModal} setShowModal={setShowModal}>
+            <Modal showModal={showModal} setShowModal={setShowModal}> {/*Modal component that will be shown when "showModal" is true. It uses showModal and setShowModal as props*/}
                 <div>
-                    Hello World
+                    Hello World                             {/*content of the pop-up panel // or "children" in this case*/}  
                     <button
                     onClick={() => setShowModal(false)}>
                         hide
@@ -25,4 +28,4 @@ function AddNewTodo(){
     )
 }
 
-export default AddNewTodo
+export default AddNewTodo // Ends the component definition, and exports the AddNewTodo component for use in other components // makes it available for other parts of the application to use
--- tdl-app/src/components/Modal.js
+++ tdl-app/src/components/Modal.js
@@ -1,17 +1,23 @@
 import React, {useRef} from 'react'
 
 
-function Modal({children, showModal, setShowModal}){
-    const modalRef = useRef()
+function Modal({children, showModal, setShowModal}) {
+    /* children is a special React prop that is used to pass elements directly into the component's output. showModal is a boolean
+    that controls the visibility of the modal, and setShowModal is a function to update this state. */
+
+    const modalRef = useRef() // initialises a ref object, which will be attached to a DOM element. "null" initially
 
     const closeModal = (e) => {
+        /* Defines a function that takes an event e as its argument. This function checks if the event's target is the outermost div (the modal background). If so, it 
+        sets showModal to false, effectively closing the modal. This typically implements a behavior where clicking on the background outside the actual modal content 
+        closes the modal.*/
         if(e.target === modalRef.current){
-            setShowModal(false)
+            setShowModal(false) // i think it means just clicking outside of the div
         }
     }
     
     return (
-        showModal &&
+        showModal && // conditional rendering statement. The modal and its contents will only render if "showModal" is true.
         <div className="Modal" ref={modalRef} onClick={closeModal}>
             <div className="container">
                 {children}
