121c61168d11c9a1141d825f3cd84002912dda24
Shi Hao Ng <shng2025@gmail.com>
2024-08-19 18:51:17 +0800

    part 19 done

--- tdl-app/src/context/index.js
+++ tdl-app/src/context/index.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react'
-import { useTodos, useProjects } from '../hooks'
+import { useTodos, useProjects, useFilterTodos } from '../hooks'
 
 
 const TodoContext = createContext()
@@ -10,14 +10,15 @@ function TodoContextProvider({children}){
 
     const todos = useTodos()
     const projects = useProjects(todos)
-
+    const filteredTodos = useFilterTodos(todos, selectedProject) // new addition, basically just acts as filtering function for base todos array, before passing it into context to be used globally as global data
+                                                                 // it uses the filter() function conditionally to filter out todos based on the selectedProject
     return (
         <TodoContext.Provider
             value={
                 {
                     selectedProject: selectedProject,
                     setSelectedProject: setSelectedProject,
-                    todos: todos,
+                    todos: filteredTodos,
                     projects: projects
                 }
             }
--- tdl-app/src/hooks/index.js
+++ tdl-app/src/hooks/index.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
-import fireDB from '../firebase' // CHANGED --> firebase.firestore() no longer valid as an operation
 
+import fireDB from '../firebase' // CHANGED --> firebase.firestore() no longer valid as an operation
 import { collection, onSnapshot } from 'firebase/firestore' // this needs to be added, since we are using newer version of firebase
+import dayjs from 'dayjs'
 
 
 export function useTodos(){
@@ -22,7 +23,7 @@ export function useTodos(){
             setTodos(data);
         });
         return () => unsubscribe();
-    }, []); // includes fireDB too, so that when there's changes made to fireDB, this function is called
+    }, []); // includes fireDB too, so that when there's changes made to fireDB, this function is called // keep empty dependency array, else function will call itself infinitely
 
     return todos;
 }
@@ -52,7 +53,7 @@ export function useProjects(todos){
                 }
             })
             setProjects(data);
-            console.log("Subscribe to Projects");
+            console.log("Subscribe to Todos");
         });
         return () => unsubscribe();
     }, []); // includes fireDB too, so that when there's changes made to fireDB, this function is called
@@ -60,6 +61,41 @@ export function useProjects(todos){
     return projects;
 }
 
+export function useFilterTodos(todos, selectedProject){         // function is exported, so it can be imported and used in other components
+    const [filteredTodos, setFilteredTodos] = useState([])      // state variables and setters, these stores the filtered list of todos. NOTICE how its initialized as an empty array
+
+    useEffect( () => {
+        let data; // let is a local variable initialisation. The keyword let is used to declare variables that are limited in scope to the block, statement, or expression on which it is used. This is 
+                  // unlike the var keyword, which defines a variable globally, or locally to an entire function regardless of block scope.
+                  // Further, this variable declared with "let" enables it to be reassigned within the different branches of conditional statements that follow, based on critieria specified by selectedProject
+
+        const todayDateFormatted = dayjs().format('MM/DD/YYYY') // switched to using dayjs instead
+                                                                // this formatted date is used to compare dates in the todo items
+
+        if (selectedProject === 'today') { // if selectedProject is "today", then we will only filter out all the "todos" thats dued today (using filter() method)
+            data = todos.filter(todo => todo.date === todayDateFormatted)
+        }
+        else if (selectedProject === 'next 7 days') { // if selectedProject is "next 7 days", then we will only filter out all the "todos" thats dued in the next 7 days
+            data = todos.filter(todo => {
+                const todoDate = dayjs(todo.date, 'MM/DD/YYYY')
+                const todayDate = dayjs(todayDateFormatted, 'MM/DD/YYYY')
+                const diffDays = todoDate.diff(todayDate, 'days')
+
+                return diffDays >=0 && diffDays < 7 // this filters out due dates which are within next 7 days from today, but also ensure that tasks from before today (in the past) are not included
+            })
+        } 
+        else if( selectedProject === 'all days') {
+            data = todos
+        } 
+        else {
+            data = todos.filter(todo => todo.projectName === selectedProject) // else, if we are not filtering by time, then we filter by projects
+        }
+
+        setFilteredTodos(data)  // at this point, data contains the filtered lists
+    }, [todos, selectedProject])
+
+    return filteredTodos
+}
 
 
 
